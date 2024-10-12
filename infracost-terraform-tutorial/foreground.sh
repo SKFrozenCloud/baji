6212a203e53cd3f0a9b49280c89a3ca24fe92a1a
SK <sk6315@live.com>
2024-10-12 22:54:04 +0200

    work

--- infracost-terraform-tutorial/foreground.sh
+++ infracost-terraform-tutorial/foreground.sh
@@ -1,2 +1,62 @@
+# Git setup
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+# Project setup
+cd /root
+mkdir /root/tutorial
 cd /root/tutorial
+mkdir terraform
+mkdir .github
+cd .github
+mkdir workflows
+cd ..
+git init
+
+GITIGNORE_CONTENT="
+# Local .terraform directories
+**/.terraform/*
+
+# .tfstate files
+*.tfstate
+*.tfstate.*
+
+# Crash log files
+crash.log
+crash.*.log
+
+# Exclude all .tfvars files, which are likely to contain sensitive data, such as
+# password, private keys, and other secrets. These should not be part of version 
+# control as they are data points which are potentially sensitive and subject 
+# to change depending on the environment.
+*.tfvars
+*.tfvars.json
+
+# Ignore override files as they are usually used to override resources locally and so
+# are not checked in
+override.tf
+override.tf.json
+*_override.tf
+*_override.tf.json
+
+# Ignore transient lock info files created by terraform apply
+.terraform.tfstate.lock.info
+
+# Include override files you do wish to add to version control using negated pattern
+# !example_override.tf
+
+# Include tfplan files to ignore the plan output of command: terraform plan -out=tfplan
+# example: *tfplan*
+
+# Ignore CLI configuration files
+.terraformrc
+terraform.rc
+"
+
+echo "$GITIGNORE_CONTENT" > .gitignore
+
+git add .
+git commit -m "Initial commit"
+
+clear
 echo "Welcome to the scenario! You will learn how to integrate Terraform and Infracost with GitHub Actions CI/CD pipeline."
