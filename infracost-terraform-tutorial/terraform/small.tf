946518787e919a7984f6fd3bf6d569450d00d4eb
Tomi Toma <ttoma@kth.se>
2024-10-14 19:22:35 +0200

    ci test

--- infracost-terraform-tutorial/terraform/small.tf
+++ infracost-terraform-tutorial/terraform/small.tf
@@ -2,7 +2,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
+
+
 resource "aws_instance" "small_server" {
   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
 }
+
+
